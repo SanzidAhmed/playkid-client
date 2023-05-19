98739cb0306fa6cac414ae23831ba6c9096aab7c
Sanzid 2.0 <sanzid.official@gmail.comm>
2023-05-19 09:00:00 +0600

    first commit

---
+++ src/components/Layout/Main.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Navbar from '../Pages/Shared/Navbar';
+
+const Main = () => {
+    return (
+        <div>
+            <div>
+                <Navbar></Navbar>
+            </div>
+            <Outlet></Outlet>
+        </div>
+    );
+};
+
+export default Main;
